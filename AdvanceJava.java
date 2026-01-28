04123bf6bfa5f236ed916eaf6521ac06ad466bfa
Pooja-chaudhary960 <pujadahit52@gmail.com>
2026-01-28 14:33:12 +0545

    Initial commit

---
+++ AdvanceJava.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.advancejava;
+import javax.swing.*;
+
+/**
+ *
+ * @author Acer
+ */
+public class AdvanceJava {
+
+    public static void main(String[] args) {
+        JFrame frame= new JFrame();
+        //Set the width and height
+        frame.setSize(400,400);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+    }
